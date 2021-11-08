89208e011190004698088313f19f4229a1f61477
GeniusPudding <r08922107@ntu.edu.tw>
2021-11-08 09:13:47 +0000

    problem 14

--- HW2/linear_regression.py
+++ HW2/linear_regression.py
@@ -12,41 +12,42 @@ def lin_reg_alg(train_data):
     # print('w_lin:',w_lin)
     return w_lin, X, y
 
-def cal_sqr_err(w_lin, X, y):
+def cal_sqr_err(w_lin, X, y, size):
     vec = np.matmul(X,w_lin) - y
-    E_in_sqr = np.dot(vec, vec)/200
+    E_in_sqr = np.dot(vec, vec)/size
     # print('E_in_sqr:',E_in_sqr)
     return E_in_sqr
 
-def cal_01_err(w_lin, X, y):
-    vec = np.sign(np.matmul(X,w_lin))
-    E_01_sqr = np.dot(vec, vec)/200
+def cal_01_err(w_lin, X, y, size):
+    E_01_sqr = np.sum(np.abs(np.sign(np.matmul(X,w_lin)) - y)/2)/size
+    # print('E_01_sqr:',E_01_sqr)    
     return E_01_sqr
 
-def problem13():
+def problem13(times):
     s = 0
-    for i in range(100):
+    for i in range(times):
         train_data, test_data = gen_dataset()
         w_lin, X, y = lin_reg_alg(train_data)
-        s += cal_sqr_err(w_lin, X, y)
-    print('av_err:',s/100)  
+        s += cal_sqr_err(w_lin, X, y,200)
+    print('av_err:',s/times)  
 
-def problem14():
-    zero_one_in_err = 0
-    zero_one_out_err = 0
-    for i in range(100):
+def problem14(times):
+    diff = 0
+    for i in range(times):
         train_data, test_data = gen_dataset()
         w_lin, X, y = lin_reg_alg(train_data)
-        zero_one_in_err += cal_01_err(w_lin, X, y)
+        zero_one_in_err = cal_01_err(w_lin, X, y,200)
 
         test_y = test_data.T[0]
         test_X = test_data.T[1:].T    
-        zero_one_out_err += cal_01_err(w_lin, test_X, test_y)
-    print('av_01_err:',zero_one_in_err/100,zero_one_out_err/100)  
+        zero_one_out_err = cal_01_err(w_lin, test_X, test_y,5000)
+        print("zero_one_in_err, zero_one_out_err:",zero_one_in_err, zero_one_out_err)
+        diff += np.abs(zero_one_in_err-zero_one_out_err)
+    print('av_01_err:',diff/times)  
 
 if __name__ == "__main__":
     #p13:
-    # problem13()
+    # problem13(100)
 
     #p14:  
-    problem14()
+    problem14(1000)#av_01_err: 0.011751800000000017
