9b53e9fb09e61eecd294e0499c73a9fe47ba12be
GeniusPudding <r08922107@ntu.edu.tw>
2021-11-08 15:17:18 +0800

    sync

--- HW2/gen_dataset.py
+++ HW2/gen_dataset.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 
 mean_pos = [2, 3]
--- HW2/linear_regression.py
+++ HW2/linear_regression.py
@@ -1,9 +1,6 @@
 from gen_dataset import *
 
-def lin_reg_alg():
-
-    train_data, test_data = gen_dataset()
-
+def lin_reg_alg(train_data):
     y = train_data.T[0]
     X = train_data.T[1:].T
     # x_pinv = []
@@ -13,13 +10,43 @@ def lin_reg_alg():
         x_pinv = np.linalg.pinv(X)
     w_lin = np.matmul(x_pinv , y )
     # print('w_lin:',w_lin)
+    return w_lin, X, y
+
+def cal_sqr_err(w_lin, X, y):
     vec = np.matmul(X,w_lin) - y
     E_in_sqr = np.dot(vec, vec)/200
     # print('E_in_sqr:',E_in_sqr)
     return E_in_sqr
 
-if __name__ == "__main__":
+def cal_01_err(w_lin, X, y):
+    vec = np.sign(np.matmul(X,w_lin))
+    E_01_sqr = np.dot(vec, vec)/200
+    return E_01_sqr
+
+def problem13():
     s = 0
     for i in range(100):
-        s += lin_reg_alg()
-    print('av_err:',s/100)    
+        train_data, test_data = gen_dataset()
+        w_lin, X, y = lin_reg_alg(train_data)
+        s += cal_sqr_err(w_lin, X, y)
+    print('av_err:',s/100)  
+
+def problem14():
+    zero_one_in_err = 0
+    zero_one_out_err = 0
+    for i in range(100):
+        train_data, test_data = gen_dataset()
+        w_lin, X, y = lin_reg_alg(train_data)
+        zero_one_in_err += cal_01_err(w_lin, X, y)
+
+        test_y = test_data.T[0]
+        test_X = test_data.T[1:].T    
+        zero_one_out_err += cal_01_err(w_lin, test_X, test_y)
+    print('av_01_err:',zero_one_in_err/100,zero_one_out_err/100)  
+
+if __name__ == "__main__":
+    #p13:
+    # problem13()
+
+    #p14:  
+    problem14()
